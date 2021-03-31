0bcd52441449db43827960980b8f1f4c07977e19
alicja <a.smolikowska@gmail.com>
2021-04-01 01:16:20 +0200

    new vc and vm

---
+++ CurrencyExchange/CurrencyCounter/ViewModel/CurrencyPickerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyPickerViewModel.swift
+//  CurrencyExchange
+//
+//  Created by Alicja Smolikowska on 01/04/2021.
+//
+
+import Foundation
---
+++ CurrencyExchange/DataModel/UserStoredRates.swift
@@ -0,0 +1,8 @@
+//
+//  UserStoredRates.swift
+//  CurrencyExchange
+//
+//  Created by Alicja Smolikowska on 31/03/2021.
+//
+
+import Foundation
